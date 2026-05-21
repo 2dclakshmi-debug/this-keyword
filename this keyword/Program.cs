f9ffd1b0140ae1bea35d541684f58e060b362951
Admin <Admin@DESKTOP-SRMBPCR>
2026-05-21 10:34:25 +0530

    Add project files.

---
+++ this keyword/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace this_keyword
+{
+    class newClass
+    {
+        int a;
+        int b;  
+        public newClass(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public void Add()
+        {
+            int c = a + b;
+            Console.WriteLine("Sum is :" + c);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            newClass nc=new newClass(10,20);
+            nc.Add();
+        }
+    }
+}
